563c7ef1ad8a72e8f191aed05ff457e75911e9fb
anduser <anduser@MacBook-Pro-andadmin.local>
2021-08-27 10:56:18 +0300

    Initial commit

---
+++ app/src/main/java/com/handen/rickandmortycompose/MainActivity.kt
@@ -0,0 +1,113 @@
+package com.handen.rickandmortycompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.handen.rickandmortycompose.ui.theme.RickAndMortyComposeTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApp {
+                MyScreenContent()
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    RickAndMortyComposeTheme {
+        Surface(color = Color.Yellow) {
+            content()
+        }
+    }
+}
+
+@Composable
+fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
+    val counterState = remember { mutableStateOf(0) }
+
+    Column(modifier = Modifier.fillMaxHeight()) {
+        Column(modifier = Modifier.weight(1f)) {
+            for (name in names) {
+                Greeting(name = name)
+                Divider(color = Color.Black)
+            }
+        }
+        Counter(
+            count = counterState.value,
+            updateCount = { newCount ->
+                counterState.value = newCount
+            }
+        )
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+}
+
+@Composable
+fun Counter(count: Int, updateCount: (Int) -> Unit) {
+    Button(
+        onClick = { updateCount(count + 1) }
+//        colors = ButtonConstants.defaultButtonColors(
+//            backgroundColor = if (count > 5) Color.Green else Color.White
+//        )
+    ) {
+        Text("I've been clicked $count times")
+    }
+}
+
+@Preview("MyScreen preview")
+@Composable
+fun DefaultPreview() {
+    MyApp {
+        MyScreenContent()
+    }
+}
+
+//class MainActivity : ComponentActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContent {
+//            RickAndMortyComposeTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(color = MaterialTheme.colors.background) {
+//                    Greeting("Android")
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//@Composable
+//fun Greeting(name: String) {
+//    Text(text = "Hello $name!")
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    RickAndMortyComposeTheme {
+//        // A surface container using the 'background' color from the theme
+//        Surface(color = MaterialTheme.colors.background) {
+//            Greeting("Android")
+//        }
+//    }
+//}
